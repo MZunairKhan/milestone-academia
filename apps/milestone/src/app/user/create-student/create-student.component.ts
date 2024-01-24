d910777e5df26e5c38f46259898d74db3969566c
MZunairKhan <khanzunair15@outlook.com>
2024-01-24 16:05:12 +0500

    Frontend: add create student component

---
+++ apps/milestone/src/app/user/create-student/create-student.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'milestone-academia-create-student',
+  templateUrl: './create-student.component.html',
+  styleUrls: ['./create-student.component.scss'],
+})
+export class CreateStudentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
