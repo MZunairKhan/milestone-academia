01bd9c0cfb683c1e6a2d7d6a6d0480d6a16af8d8
Haseeb <mhsharif@bytecloudsolutions.com>
2024-05-09 17:57:45 +0500

    Updated

--- build-and-run.js
+++ build-and-run.js
@@ -1,18 +1,45 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-async function buildAndRun() {
+async function executeCommand(command) {
   try {
-    await exec('nx run milestone:build:production');
-
-    await exec('nx run api:build');
-
-    await exec('docker-compose up');
-
-    console.log('Build and run process completed successfully.');
+    console.log('Running CMD:', command);
+    const { stdout } = await exec(command); 
+    return stdout;
   } catch (error) {
-    console.error('Error occurred:', error);
+    console.error('Error executing command:', error.message);
+    throw error;
   }
 }
 
-buildAndRun();
+async function buildAndRun() {
+  try {
+    console.log('Building Frontend...');
+    const buildMilestone = await executeCommand('nx build milestone');
+    console.log('Frontend Build Completed Successfully', buildMilestone);
+
+    console.log('Building Backend...');
+    const buildApi = await executeCommand('nx build api');
+    console.log('Backend Build Completed Successfully', buildApi);
+
+    console.log('Starting containers...');
+    const dockerUpOutput = await executeCommand('docker-compose up --detach');
+    console.log('Containers started successfully', dockerUpOutput);
+
+    
+    console.log('Waiting for containers to initialize (20 seconds)...');
+    await new Promise(resolve => setTimeout(resolve, 20000)); 
+
+    console.log('Getting Logs');
+    const logs = await executeCommand('docker-compose logs --tail=10')
+    console.log('Container Logs', logs);
+
+  } catch (error) {
+    console.error('Error occurred during build and run process:', error.message);
+    throw error;
+  }
+}
+
+buildAndRun()
+  .then(() => console.log('Script execution completed.'))
+  .catch(error => console.error('Script execution failed:', error.message));
